73c0d60c5d152b32189f7246b598ee8e1c894295
Sazzad Hossain <sazzadhossainadib@gmail.com>
2025-09-04 23:17:12 +0600

    clerk auth full setup and store user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-
 import { motion } from "motion/react";
 import { FeatureBentoGrid } from "./_components/FeatureBentoGrid";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { div } from "motion/react-client";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -19,23 +22,21 @@ export default function Home() {
       </div>
       <div className="px-4 py-10 md:py-20">
         <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Medical Service By AI"
-            .split(" ")
-            .map((word, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
-                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
-                transition={{
-                  duration: 0.3,
-                  delay: index * 0.1,
-                  ease: "easeInOut",
-                }}
-                className="mr-2 inline-block"
-              >
-                {word}
-              </motion.span>
-            ))}
+          {"Medical Service By AI".split(" ").map((word, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+              animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+              transition={{
+                duration: 0.3,
+                delay: index * 0.1,
+                ease: "easeInOut",
+              }}
+              className="mr-2 inline-block"
+            >
+              {word}
+            </motion.span>
+          ))}
         </h1>
         <motion.p
           initial={{
@@ -50,8 +51,11 @@ export default function Home() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-         Leverage cutting-edge AI technology to receive reliable, personalized health advice. Whether you're feeling under the weather or curious about your symptoms, our AI Doctor Agent is here to help.
+          Leverage cutting-edge AI technology to receive reliable, personalized
+          health advice. Whether you're feeling under the weather or curious
+          about your symptoms, our AI Doctor Agent is here to help.
         </motion.p>
+        <Link href={'/sign-in'}>
         <motion.div
           initial={{
             opacity: 0,
@@ -64,14 +68,12 @@ export default function Home() {
             delay: 1,
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
-        >
+          >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Explore Now
-          </button>
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            Contact Support
+            Get Started
           </button>
         </motion.div>
+        </Link>
         <motion.div
           initial={{
             opacity: 0,
@@ -98,25 +100,31 @@ export default function Home() {
           </div>
         </motion.div>
       </div>
-      <FeatureBentoGrid/>
+      <FeatureBentoGrid />
     </div>
   );
 }
 
 const Navbar = () => {
+  const { user } = useUser();
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
         <h1 className="text-base font-bold md:text-2xl">AI DOC</h1>
       </div>
-      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-        Login
-      </button>
+      {!user ? (
+        <Link href={'/sign-in'}>
+        <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+          Login
+        </button>
+        </Link>
+      ) : (
+        <div className="flex gap-5 items-center">
+          <UserButton />
+          <Button>Dashboard</Button>
+        </div>
+      )}
     </nav>
   );
 };
-
-
-
-
