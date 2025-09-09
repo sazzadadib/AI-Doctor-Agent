cbf00421b6864d1280ae9d9121f50cd656fe177e
Sazzad Hossain <sazzadhossainadib@gmail.com>
2025-09-09 12:28:21 +0600

    error fixed and simple version ready for production

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ import { UserButton, useUser } from "@clerk/nextjs";
 import { div } from "motion/react-client";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const { user } = useUser();
   return (
     <div className="relative my-10 flex flex-col items-center justify-center">
       <Navbar />
@@ -55,26 +57,35 @@ export default function Home() {
           health advice. Whether you're feeling under the weather or curious
           about your symptoms, our AI Doctor Agent is here to help.
         </motion.p>
-        <Link href={'/sign-in'}>
-        <motion.div
-          initial={{
-            opacity: 0,
-          }}
-          animate={{
-            opacity: 1,
-          }}
-          transition={{
-            duration: 0.3,
-            delay: 1,
-          }}
-          className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
-          >
-          <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Get Started
-          </button>
-        </motion.div>
-        </Link>
-        <motion.div
+        {!user ? (
+          <Link href="/sign-in">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3, delay: 1 }}
+              className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+            >
+              <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+                Get Started
+              </button>
+            </motion.div>
+          </Link>
+        ) : (
+          <Link href="/dashboard">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.3, delay: 1 }}
+              className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+            >
+              <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+                Go to Dashboard
+              </button>
+            </motion.div>
+          </Link>
+        )}
+
+        {/* <motion.div
           initial={{
             opacity: 0,
             y: 10,
@@ -98,9 +109,9 @@ export default function Home() {
               width={1000}
             />
           </div>
-        </motion.div>
+        </motion.div> */}
       </div>
-      <FeatureBentoGrid />
+      {/* <FeatureBentoGrid /> */}
     </div>
   );
 }
@@ -110,19 +121,20 @@ const Navbar = () => {
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
-        <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
-        <h1 className="text-base font-bold md:text-2xl">AI DOC</h1>
+         <Image src={"/logo.png"} alt="logo" width={180} height={90} />
       </div>
       {!user ? (
-        <Link href={'/sign-in'}>
-        <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-          Login
-        </button>
+        <Link href={"/sign-in"}>
+          <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+            Login
+          </button>
         </Link>
       ) : (
         <div className="flex gap-5 items-center">
           <UserButton />
-          <Button>Dashboard</Button>
+          <Link href={"/dashboard"}>
+            <Button>Dashboard</Button>
+          </Link>
         </div>
       )}
     </nav>
